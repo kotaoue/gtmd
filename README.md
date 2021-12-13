5b2b51f518ee5366c8576e7ff9cf070f3940d4ed
kota oue <kota.oue@yappli.co.jp>
2021-12-13 23:10:05 +0900

    create tool

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gtmd
 Get the title tag and make markdown.
+
+## Usage
+```ShellSession
+$ go run main.go -url=https://go.dev/play/
+
+$ cat "Go Playground - go.dev.md"
+# [Go Playground - go.dev](https://go.dev/play/)
+```
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+var (
+	rawurl string
+)
+
+func init() {
+	flag.StringVar(&rawurl, "url", "https://pkg.go.dev/", "source url")
+	flag.Parse()
+}
+
+func main() {
+	if err := Main(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func Main() error {
+	n, err := node(rawurl)
+	if err != nil {
+		return err
+	}
+
+	t := pageTitle(n)
+
+	if err := touch(rawurl, t); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func node(s string) (*html.Node, error) {
+	u, err := url.Parse(s)
+	if err != nil {
+		return nil, fmt.Errorf("can't parse url")
+	}
+
+	r, err := http.Get(u.String())
+	if err != nil {
+		return nil, fmt.Errorf("can't get page")
+	}
+
+	page, err := html.Parse(r.Body)
+	if err != nil {
+		return nil, fmt.Errorf("can't parse page")
+	}
+	return page, nil
+}
+
+func pageTitle(n *html.Node) (title string) {
+	if n.Type == html.ElementNode && n.Data == "title" {
+		return n.FirstChild.Data
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		title = pageTitle(c)
+		if title != "" {
+			break
+		}
+	}
+	return title
+}
+
+func touch(rawurl, title string) error {
+	fp, err := os.Create(fmt.Sprintf("%s.md", title))
+	if err != nil {
+		return err
+	}
+	defer fp.Close()
+
+	fp.WriteString(fmt.Sprintf("# [%s](%s)", title, rawurl))
+
+	return nil
+}
